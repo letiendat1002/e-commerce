ebaaefaf5b51cc84d9857b296a830db67cd71cdc
Nguyễn Trung Quốc <113247967+qnt078@users.noreply.github.com>
2023-03-12 09:00:49 +0700

    add code product detail and cart page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,18 @@ import Footer from "./layouts/Footer";
 import Header from "./layouts/Header";
 import Home from "./page/Home";
 import Menu from "./page/Menu";
+import ProductDetail from "./page/ProductDetail";
+import Cart from "./page/Cart";
 
 function App() {
   return (
     <div className="App">
       <Header />
       {/* <Home /> */}
-      <Menu />
-      <Footer />
+      {/* <Menu /> */}
+      {/* <ProductDetail /> */}
+      <Cart />
+      {/* <Footer /> */}
     </div>
   );
 }
