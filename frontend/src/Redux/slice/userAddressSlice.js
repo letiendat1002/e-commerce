fa0049053013b67c62392231d17902a924ec7291
trandangnguyenbao <trandangnguyenbao2810@gmail.com>
2023-05-07 13:50:27 +0700

    REFACTOR: Update code Add Address

--- frontend/src/Redux/slice/userAddressSlice.js
+++ frontend/src/Redux/slice/userAddressSlice.js
@@ -17,6 +17,17 @@ export const getUserAddressForIDUser = createAsyncThunk('getUserAddressForIDUser
     }
 });
 
+export const addAddress = createAsyncThunk('addAddress', async(data) => {
+    try {
+        const response = await axiosClient4.post('useraddresses', data)
+        return response
+    }
+    catch(error){
+        console.log("error: ", error)
+        throw error
+    }
+})
+
 export const addressSlice = createSlice({
     name: "userAddress", 
     initialState,
@@ -30,6 +41,16 @@ export const addressSlice = createSlice({
             state.data = action.payload
         })
         builder.addCase(getUserAddressForIDUser.rejected, (state, action) => {
+            state.loading = true
+        })
+        builder.addCase(addAddress.pending, (state, action) => {
+            state.loading = true
+        })
+        builder.addCase(addAddress.fulfilled, (state, action) => {
+            state.loading = false
+            state.data = action.payload
+        })
+        builder.addCase(addAddress.rejected, (state, action) => {
             state.loading = true
         })
     }
